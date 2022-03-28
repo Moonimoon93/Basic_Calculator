e25a052c11573e39539bafd7bf4f7082252639db
Moonimoon93 <harrygularski@gmail.com>
2022-03-28 15:39:31 +0900

    Two concepts for invoking actual functions when user press '=' button. 1. Use screen as temporal place to store numbers and operators. 2. Make a behind the scene algorithm that calculate as user types in real time     a. Problem : calculator will mess its order of operation if user press '*' button later than '+' button.
    IN PROCESS : Pursuing solution with #1 concept.

--- script.js
+++ script.js
@@ -19,13 +19,36 @@ const operate = (operator, num1, num2) => {
 }
 const screen = document.querySelector('.screen');
 
+//When digits are pressed, numbers will be added at the end.
+//Except the beginning. 0 that is originally shown on screen will be replaced first.
+//Then, numbers will be added at the end as digits are pressed.
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => {
     number.addEventListener('click', () => {
         if (screen.innerText === '0') {
+            //Initially, replacing 0 with first number.
             screen.innerHTML = number.value
         } else {
+            //In order to put number at the end rather replacing what's on screen.
             screen.insertAdjacentHTML("beforeend", number.value)
         }
     })
 });
+
+
+//Concept 1
+//1. Numbers and operators will be shown on screen when user pressed buttons.
+//2. When user press '=' button, there will be a logic to 
+//  a. identify numbers and operator(s)
+//  b. invoke appopriate operator(s)
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        screen.insertAdjacentHTML("beforeend", operator.value)
+    })
+})
+
+//Concept 2
+//1. Operators will be invoked behind the scence as user press operator button.
+//This concept has a critical problem, if user press '+' and '*' later
+//proper order of operation will be ruined.
